08c92e73e190d178f74715509aee078fd9580fee
rushisyuya0125-beep <rushi.syuya0125@gmail.com>
2026-06-26 14:06:48 +0900

    creators: replace Rainy with 5 players (buyuriru/rizart/captainshota/koyota/ragis)

--- lib/creatorsData.ts
+++ lib/creatorsData.ts
@@ -15,50 +15,65 @@ export interface Player {
   sns: SnsLinks;
 }
 
-export interface Team {
-  id: string;
-  name: string;
-  image: string;
-  description: string;
-  primaryUrl: string;
-  sns: SnsLinks;
-}
-
 export const PLAYERS: Player[] = [
   {
-    id: "rainy",
-    name: "Rainy",
-    image: "https://unavatar.io/x/R4InyFN",
+    id: "buyuriru",
+    name: "ぶゅりる",
+    image: "https://unavatar.io/x/Buyuriru",
     team: "DFM",
-    description: "DFM所属。日本トップクラスの競技プレイヤー。",
-    primaryUrl: "https://x.com/R4InyFN",
+    description: "アジア1位・世界大会出場の元DFMプロ。",
+    primaryUrl: "https://x.com/Buyuriru",
     sns: {
-      x: "https://x.com/R4InyFN",
-      youtube: "https://www.youtube.com/channel/UCOK3b_q7lYy2k1TCR2U2_kw",
-    },
-  },
-];
-
-export const TEAMS: Team[] = [
-  {
-    id: "dfm",
-    name: "DetonatioN FocusMe",
-    image: "/teams/dfm.png",
-    description: "2012年設立の日本老舗eスポーツ組織。複数タイトルで国内トップ。",
-    primaryUrl: "https://x.com/team_detonation",
-    sns: {
-      x: "https://x.com/team_detonation",
-      youtube: "https://www.youtube.com/@DetonatioNFocusMe",
+      x: "https://x.com/Buyuriru",
+      youtube: "https://www.youtube.com/@buyuriru",
     },
   },
   {
-    id: "edge",
-    name: "EDGE",
-    image: "https://unavatar.io/x/EDGE_CPT",
-    description: "日本のフォートナイトプロチーム。競技シーンで活躍中。",
-    primaryUrl: "https://x.com/EDGE_CPT",
+    id: "rizart",
+    name: "リズアート",
+    image: "https://unavatar.io/x/CRRIZARTTT",
+    team: "Crazy Raccoon",
+    description: "世界1位34キル達成。元CRのレジェンド。",
+    primaryUrl: "https://x.com/CRRIZARTTT",
     sns: {
-      x: "https://x.com/EDGE_CPT",
+      x: "https://x.com/CRRIZARTTT",
+      youtube: "https://www.youtube.com/channel/UCWcS5FzBnhiKm27NJNCB9Qw",
+    },
+  },
+  {
+    id: "captainshota",
+    name: "キャプテンしょーた",
+    image: "https://unavatar.io/x/Captain_Shota",
+    team: "Circus",
+    description: "60万人超のフォートナイトYouTuber。",
+    primaryUrl: "https://x.com/Captain_Shota",
+    sns: {
+      x: "https://x.com/Captain_Shota",
+      youtube: "https://www.youtube.com/@cptshota",
+    },
+  },
+  {
+    id: "koyota",
+    name: "Koyota",
+    image: "https://unavatar.io/x/Koyota0",
+    team: "ZETA DIVISION",
+    description: "FNCS 2024優勝。アジアトップの競技選手。",
+    primaryUrl: "https://x.com/Koyota0",
+    sns: {
+      x: "https://x.com/Koyota0",
+      youtube: "https://www.youtube.com/channel/UCJ5e3yAt_Wg9tJGjFO0S-xQ",
+    },
+  },
+  {
+    id: "ragis",
+    name: "Ragis",
+    image: "https://unavatar.io/x/rag1sfn",
+    team: "Nova",
+    description: "アジアソロ1位。最強の若手プロ。",
+    primaryUrl: "https://x.com/rag1sfn",
+    sns: {
+      x: "https://x.com/rag1sfn",
+      youtube: "https://www.youtube.com/@ragis6490",
     },
   },
 ];
